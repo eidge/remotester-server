7c40a8c43a332a46b941db71787b21906d265217
Hugo Ribeira <hugoribeira@gmail.com>
2014-12-10 23:14:55 +0000

    Clean up express scaffold since we re not using views

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -15,7 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
