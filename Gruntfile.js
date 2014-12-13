e1a180e224c9ea22a3cdae48e5b889082279a6c0
Hugo Ribeira <hugoribeira@gmail.com>
2014-12-13 23:49:57 +0000

    Add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+  grunt.registerTask('db:seed', 'Seed database', function(){
+    var done   = this.async();
+    var models = require(__dirname + '/models');
+    models
+      .User
+      .findOrCreate({
+        where: {
+          email: 'hugoribeira@gmail.com',
+          name: 'Hugo Ribeira',
+          token: 'c3b6a0c402b26c8669175fb5b84d1c8389fe210297236b0b635aafbee5bd05ad248762ab749eee5a0edda0c6d4b744d9'
+        }
+      })
+      .complete(function(err) {
+        done(err === null);
+      });
+  });
+
+  grunt.registerTask('db:drop', 'Seed database', function(){
+    var models = require(__dirname + '/models');
+    models.User.drop();
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', []);
+
+};
